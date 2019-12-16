15ab9e52a1ad3446f97c64d6987e1b07c0acf421
Bruno Fillion <Bruno.Fillion@ia.ca>
2019-12-16 15:50:15 -0500

    script

--- ssl/ksql/ksql-script.sql
+++ ssl/ksql/ksql-script.sql
@@ -3,3 +3,17 @@ CREATE STREAM reponse (idCorrelation VARCHAR, reponse VARCHAR, fonction VARCHAR)
           partitions = 5,
           key = 'idCorrelation',
           value_format = 'avro');
+
+CREATE SINK CONNECTOR elasticsearch_sink WITH (
+  'connector.class'     = 'io.confluent.connect.elasticsearch.ElasticsearchSinkConnector',
+  'key.converter'       = 'org.apache.kafka.connect.storage.StringConverter',
+  'topics'              = 'reponse',
+  'key.ignore'          = 'true',
+  'schema.ignore'       = 'false',
+  'type.name'           = 'reponses',
+  'connection.url'      = 'http://elastic:9200',
+  'connection.username' = 'elastic',
+  'connection.password' = 'secret');
+
+
+
